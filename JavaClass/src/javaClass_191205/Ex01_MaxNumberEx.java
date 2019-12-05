af5b17afaff81f1f7d084669fd5f8d2fb97c6f70
1 <1@1-PC>
2019-12-05 10:00:09 +0900

    MaxNumber 세개 숫자

--- JavaClass/src/javaClass_191205/Ex01_MaxNumberEx.java
+++ JavaClass/src/javaClass_191205/Ex01_MaxNumberEx.java
@@ -8,16 +8,25 @@ public class Ex01_MaxNumberEx {
 		// 두 수를 입력받아 큰 수를 출력하시오. 
 		// 응용 : 3개 받아서 큰 수 출력 
 		Scanner scan = new Scanner(System.in);
-		int num1, num2, max;
+		int num1, num2, num3, max;
 		System.out.print("첫번째 숫자 입력 : ");
 		num1 = scan.nextInt();
 		System.out.print("두번째 숫자 입력 : ");
 		num2 = scan.nextInt();
-
+		System.out.print("세번째 숫자 입력 : ");
+		num3 = scan.nextInt();
 		if(num1>num2) {
-			max = num1;
+			if(num1>num3) {
+				max = num1;
+			} else {
+				max = num3;
+			}
 		} else {
-			max = num2;
+			if(num2>num3) {
+				max = num2;
+			} else {
+				max = num3;
+			}
 		}
 		System.out.println("큰 수는 "+max +"입니다.");
 		
